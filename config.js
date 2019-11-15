7caa6b6dbd7d44da75966e172f9a081424780bf8
p4k4 <43571687+p4k4@users.noreply.github.com>
2019-11-15 15:30:14 +1300

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+var config = {
+  // instance
+  localhost: {
+    // config
+    server: {
+      // hostname or ip address
+      address: 'ipaddress'
+    },
+    // rcon
+    rcon: {
+      // rcon port
+      port: 'rconport',
+      // rcon password
+      password: 'password'
+    }
+  }
+};
+
+
+module.exports = config;
